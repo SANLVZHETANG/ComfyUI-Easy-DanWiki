a25761dfe4acbcf2c81af2af9a7b6e60f1f89384
checkpoint <ck@local>
2026-09-04 23:10:37 +0800

    CP37: duo fills full width (flex 1 1 0, no content-width cap), list stretches to panel height - canyon gone

--- src/web/js/dbtags_autocomplete.js
+++ src/web/js/dbtags_autocomplete.js
@@ -10,7 +10,7 @@ import { $el } from "../../../scripts/ui.js";
 // stylesheet: load dbtags_autocomplete.css (same dir as this file)
 {
 	const url = new URL("./dbtags_autocomplete.css", import.meta.url);
-	url.search = "?v=cp36";
+	url.search = "?v=cp37";
 	$el("link", { parent: document.head, rel: "stylesheet", type: "text/css", href: url });
 }
 
